da5fb6f8d7ddd7d12a9df5a0fa5586dfd3126e33
Michael Pantazoglou <79846027+mikepantazoglou@users.noreply.github.com>
2022-11-29 17:24:05 +0200

    Add support for enabling/disabling printing out the JavaScript console logs to stdout (#69)
    Co-authored-by: alkis-gkouzias-obs <alkis.gkouzias@oceanbluesoftware.co.uk>

--- rdk/ORBBrowser/WebKitImplementation.cpp
+++ rdk/ORBBrowser/WebKitImplementation.cpp
@@ -2458,6 +2458,12 @@ private:
                 "HbbTV/1.6.1 (; OBS; WPE; v1.0.0-alpha; OBS;)");
         }
 
+        if (_config.LogToSystemConsoleEnabled.IsSet() == true)
+        {
+            webkit_settings_set_enable_write_console_messages_to_stdout(preferences,
+                _config.LogToSystemConsoleEnabled.Value());
+        }
+
         _view = WEBKIT_WEB_VIEW(g_object_new(WEBKIT_TYPE_WEB_VIEW,
             "backend", webkit_web_view_backend_new(wpe_view_backend_create(), nullptr, nullptr),
             "web-context", context,
