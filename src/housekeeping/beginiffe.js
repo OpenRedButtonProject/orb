ba00a33568cb5c51012112a33e7790bae642804c
giorgos.epitropou <giorgos.epitropou@oceanbluesoftware.co.uk>
2022-12-14 11:33:02 +0000

    keep a URL reference in case a test/portal overrides the native javascript object add a setting in order to change the dash.js debug level

--- src/housekeeping/beginiffe.js
+++ src/housekeeping/beginiffe.js
@@ -2,3 +2,6 @@
 
       "use strict";
       let hbbtv = {};
+
+      const __URL = URL;
+      //Object.freeze(__URL);
--- src/mediaproxies/dashproxy.js
+++ src/mediaproxies/dashproxy.js
@@ -571,6 +571,16 @@ hbbtv.objects.DashProxy = (function() {
          p.player = orb_dashjs.MediaPlayer().create();
          p.player.registerCustomCapabilitiesFilter(filterCapabilities.bind(this));
          p.player.updateSettings({
+            debug: {
+               //LOG_LEVEL_NONE     0
+               //LOG_LEVEL_FATAL    1
+               //LOG_LEVEL_ERROR    2
+               //LOG_LEVEL_WARNING  3
+               //LOG_LEVEL_INFO     4
+               //LOG_LEVEL_DEBUG    5
+               logLevel: 3,
+               dispatchEvent: false
+            },
             streaming: {
                trackSwitchMode: {
                   audio: 'alwaysReplace',
