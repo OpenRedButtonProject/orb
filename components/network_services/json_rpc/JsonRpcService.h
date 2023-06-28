87bb305469d14602776947ac9302136542462501
ivanqin-obs <ivan.qin@oceanbluesoftware.co.uk>
2023-06-28 09:57:09 +0100

    update JsonRpcService

--- components/network_services/json_rpc/JsonRpcService.h
+++ components/network_services/json_rpc/JsonRpcService.h
@@ -13,40 +13,169 @@
 #include <string>
 #include <unordered_map>
 #include <mutex>
+#include <json/json.h>
 
 namespace NetworkServices {
-class JsonRpcService : public WebSocketService {
-public:
-    class SessionCallback {
-public:
+    class JsonRpcService : public WebSocketService {
+    public:
+        class SessionCallback {
+    public:
         virtual void RequestDialogueEnhancementOverride(
             int connectionId,
             int id,
             int dialogueEnhancementGain) = 0;
 
-        virtual ~SessionCallback() = default;
+            virtual ~SessionCallback() = default;
+        };
+
+        JsonRpcService(int port, const std::string &endpoint,
+            std::unique_ptr<SessionCallback> m_sessionCallback);
+
+        bool OnConnection(WebSocketConnection *connection) override;
+
+        void OnMessageReceived(WebSocketConnection *connection, const std::string &text) override;
+
+        void OnDisconnected(WebSocketConnection *connection) override;
+
+        void OnServiceStopped() override;
+
+        void RespondError(int connectionId, const std::string &id, int code, const std::string &message,
+                          const std::string &method);
+
+        void RespondError(int connectionId, const std::string &id, int code, const std::string &message);
+
+        void RespondNegotiateMethods(int connectionId, const std::string &id,
+                                const std::string &terminalToApp,
+                                const std::string &appToTerminal);
+
+        void RespondSubscribe(int connectionId, const std::string &id, bool msgType0, bool msgType1,
+                              bool msgType2, bool msgType3, bool msgType4, bool msgType5, bool msgType6,
+                              bool msgType7);
+
+        void RespondFeatureSuppress(int connectionId, const std::string &id, const std::string &feature,
+                                    const std::string &value);
+
+        void RespondFeatureSettingsInVisionSigning(int connectionId, const std::string &id, bool enabled);
+
+        void RespondFeatureSettingsAudioDescription(int connectionId, const std::string &id, bool enabled,
+                                               int gainPreference, int panAzimuthPreference);
+
+        void RespondFeatureSettingsResponseToUserAction(int connectionId, const std::string &id,
+                                                   bool enabled,
+                                                   const std::string &type);
+
+        void RespondFeatureSettingsScreenReader(int connectionId, const std::string &id, bool enabled,
+                                           int speed,
+                                           const std::string &voice, const std::string &language);
+
+        void RespondFeatureSettingsHighContrastUI(int connectionId, const std::string &id, bool enabled,
+                                                  const std::string &hcType);
+
+        void RespondFeatureSettingsUiMagnifier(int connectionId, const std::string &id, bool enabled,
+                                               const std::string &magType);
+
+        void RespondFeatureSettingsQueryDialogueEnhancement(int connectionId, const std::string &id,
+                                                            int dialogueEnhancementGainPreference,
+                                                            int dialogueEnhancementGain,
+                                                            int dialogueEnhancementLimitMin,
+                                                            int dialogueEnhancementLimitMax);
+
+        void RespondFeatureSettingsQuerySubtitles(int connectionId, const std::string &id, bool enabled,
+                                                  int size, const std::string &fontFamily,
+                                                  const std::string &textColour, int textOpacity,
+                                                  const std::string &edgeType,
+                                                  const std::string &edgeColour,
+                                                  const std::string &backgroundColour,
+                                                  int backgroundOpacity,
+                                                  const std::string &windowColour,
+                                                  int windowOpacity, const std::string &language);
+
+        void RespondFeatureSupportInfo(int connectionId, const std::string &id, const std::string &feature,
+                                  const std::string &value);
+
+        void
+        SendIntentMediaPause(int connectionId, const std::string &id, const std::string &origin);
+
+        void
+        SendIntentMediaPlay(int connectionId, const std::string &id, const std::string &origin);
+
+        void SendIntentMediaFastForward(int connectionId, const std::string &id,
+                                        const std::string &origin);
+
+        void SendIntentMediaFastReverse(int connectionId, const std::string &id,
+                                        const std::string &origin);
+
+        void
+        SendIntentMediaStop(int connectionId, const std::string &id, const std::string &origin);
+
+        void SendIntentMediaSeekContent(int connectionId, const std::string &id,
+                                        const std::string &origin,
+                                        const std::string &anchor, int offset);
+
+        void SendIntentMediaSeekRelative(int connectionId, const std::string &id,
+                                         const std::string &origin,
+                                         const std::string &anchor, int offset);
+
+        void
+        SendIntentMediaSeekLive(int connectionId, const std::string &id, const std::string &origin,
+                                int offset);
+
+        void
+        SendIntentMediaSeekWallclock(int connectionId, const std::string &id,
+                                     const std::string &origin,
+                                     const std::string &dateTime);
+
+        void SendIntentSearch(int connectionId, const std::string &id, const std::string &origin,
+                              const std::string &query);
+
+        void SendIntentDisplay(int connectionId, const std::string &id, const std::string &origin,
+                               const std::string &mediaId);
+
+        void SendIntentPlayback(int connectionId, const std::string &id, const std::string &origin,
+                                const std::string &mediaId, const std::string &anchor, int offset);
+
+        void NotifyInVisionSigning(int connectionId, const std::string &id, bool enabled);
+
+        void
+        NotifyAudioDescription(int connectionId, const std::string &id, bool enabled,
+                               int gainPreference,
+                               int panAzimuthPreference);
+
+        void NotifyResponseToUserAction(int connectionId, const std::string &id, bool enabled,
+                                        const std::string &type);
+
+        void NotifyScreenReader(int connectionId, const std::string &id, bool enabled, int speed,
+                                const std::string &voice, const std::string &language);
+
+        void NotifyHighContrastUI(int connectionId, const std::string &id, bool enabled,
+                                  const std::string &hcType);
+
+        void NotifyUIMagnifier(int connectionId, const std::string &id, bool enabled,
+                               const std::string &magType);
+
+        void NotifyDialogueEnhancement(int connectionId, const std::string &id,
+                                       int dialogueEnhancementGainPreference,
+                                       int dialogueEnhancementGain,
+                                       int dialogueEnhancementLimitMin,
+                                       int dialogueEnhancementLimitMax);
+
+        void NotifySubtitles(int connectionId, const std::string &id, bool enabled, int size,
+                             const std::string &fontFamily, const std::string &textColour,
+                             int textOpacity,
+                             const std::string &edgeType, const std::string &edgeColour,
+                             const std::string &backgroundColour, int backgroundOpacity,
+                             const std::string &windowColour, int windowOpacity,
+                             const std::string &language);
+        void RespondDialogueEnhancementOverride(int connectionId, const std::string &id,
+                                                int dialogueEnhancementGain);
+
+        void
+        RespondTriggerResponseToUserAction(int connectionId, const std::string &id, bool actioned);
+    private:
+        std::string m_endpoint;
+        std::unique_ptr<SessionCallback> m_sessionCallback;
+
     };
-    
-    JsonRpcService(int port, const std::string &endpoint,
-        std::unique_ptr<SessionCallback> m_sessionCallback);
-
-    bool OnConnection(WebSocketConnection *connection) override;
-
-    void OnMessageReceived(WebSocketConnection *connection, const std::string &text) override;
-
-    void OnDisconnected(WebSocketConnection *connection) override;
-
-    void OnServiceStopped() override;
-
-    void RespondDialogueEnhancementOverride(
-        int connectionId,
-        int id,
-        int dialogueEnhancementGain);
-
-private:
-    std::string m_endpoint;
-    std::unique_ptr<SessionCallback> m_sessionCallback;
-};
 } // namespace NetworkServices
 
 #endif // OBS_NS_JSONRPCSERVICE_
