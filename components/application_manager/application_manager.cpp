d6a94419733cd71969b35ccda4f9da857ed0f829
Marios Christoforakis <marios.christoforakis@oceanbluesoftware.co.uk>
2023-10-20 14:58:49 +0300

    HTM-243: Fix for faulty condition in 6b45a4e3581a78ac56c75308267afc00821ddebf.

--- components/application_manager/application_manager.cpp
+++ components/application_manager/application_manager.cpp
@@ -263,7 +263,7 @@ uint16_t ApplicationManager::SetKeySetMask(uint16_t appId, uint16_t keySetMask)
     std::lock_guard<std::recursive_mutex> lock(m_lock);
     if (m_app.id == appId)
     {
-        if (!m_app.isActivated && (m_ait.Get()->scheme != LINKED_APP_SCHEME_1_2 || m_ait.Get()->scheme != LINKED_APP_SCHEME_2))
+        if (!m_app.isActivated && m_ait.Get()->scheme != LINKED_APP_SCHEME_1_2 && m_ait.Get()->scheme != LINKED_APP_SCHEME_2)
         {
             if ((keySetMask & KEY_SET_VCR) != 0)
             {
