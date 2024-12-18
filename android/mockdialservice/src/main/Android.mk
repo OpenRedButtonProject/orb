06e442b858d9c4e8616c31f71356f60bfb7ea94c
AdamSturtridge <adam.sturtridge@oceanbluesoftware.co.uk>
2024-12-18 18:55:41 +0000

    Avoid using symlinks

--- android/mockdialservice/src/main/Android.mk
+++ android/mockdialservice/src/main/Android.mk
@@ -20,5 +20,9 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
-include $(call all-makefiles-under, $(LOCAL_PATH))
+
+ORB_COMPONENT_PATH := $(LOCAL_PATH)/../../../../components
+#include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(LOCAL_PATH)/cpp/Android.mk
+include $(ORB_COMPONENT_PATH)/mock_dial_service/Android.mk
 
