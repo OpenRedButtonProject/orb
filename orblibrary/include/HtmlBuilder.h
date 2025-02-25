95a3f350d96419dc866c88179b0659e3c1c75c57
DavidFee-OBS <david.fee@oceanbluesoftware.co.uk>
2025-02-25 16:04:31 +0000

    FREE-62 Create polyfill js (#364)
    * FREE-62 Initial BUILD.gn to concatenate iframe.js sources.
    
    * FREE-62 Adding remaining polyfill dependencies from BuildPolyfill.mk
    
    * FREE-62 hbbtv.js is built into a shared lib.
    
    * FREE-62 Javascript is built as static lib
    Unit test is added to check for Javascript static lib.
    
    * FREE-62 Creating a polyfill library based on HtmlBuilder.java
    
    * FREE-62 HbbTV js injection adds script tags

---
+++ orblibrary/include/HtmlBuilder.h
@@ -0,0 +1,28 @@
+#ifndef ORB_HTML_BUILDER_H
+#define ORB_HTML_BUILDER_H
+
+#include <string>
+#include <string_view>
+
+namespace orb::polyfill
+{
+class HtmlBuilder
+{
+public:
+  static constexpr std::string_view script_tag_open = "<script type=\"text/javascript\">\n//<![CDATA[\n";
+  static constexpr std::string_view script_tag_close = "\n//]]>\n</script>";
+
+  HtmlBuilder();
+  ~HtmlBuilder() = default;
+
+  static const std::string getHbbtvJs();
+
+  const std::string getHbbtvInjection();
+
+  // TODO Add remaining items:
+  // See android/orblibrary/src/main/java/org/orbtv/orblibrary/HtmlBuilder.java
+};
+
+} // namespace orb::polyfill
+
+#endif // ORB_HTML_BUILDER_H
